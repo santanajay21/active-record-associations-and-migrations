bb241df3bb2248ba701efa7fbadb73f6965b9716
jeremysantana <santanajay21@gmail.com>
2020-12-31 13:26:52 -0500

    commit tree 2386f3dc45ec04c3dc079d3090f09f9c37d8a9b5

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,21 @@
 class Artist < ActiveRecord::Base
+  has_many :songs
+  has_many :genres, through: :songs
+
+  def get_genre_of_first_song
+    #return the genre of the artist's first saved song
+    self.first.songs.genre 	    
+  end	  
+
+
+  def song_count	  
+    #return the number of songs associated with the artist	 
+    self.song.count  
+  end	  
+
+
+  def genre_count	  
+    #return the number of genres associated with the artist	  
+    self.genre.count  
+  end	  
 end
